cabd8522d151809a34284a5a780dc55bbedc8af9
cake2 <altyaboi449@gmail.com>
2024-12-22 14:10:14 -0500

    First commit - hope this works

---
+++ src/main/java/net/yaboiiii_xd/voidcrazed/VoidcrazedClient.java
@@ -0,0 +1,10 @@
+package net.yaboiiii_xd.voidcrazed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoidcrazedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/yaboiiii_xd/voidcrazed/VoidcrazedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yaboiiii_xd.voidcrazed;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VoidcrazedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
